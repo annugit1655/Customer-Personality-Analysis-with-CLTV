02e0f2f86b9c8e04f23b927e368e08a2da6d35ab
ayannu <ayannu1655@gmail.com>
2026-06-05 19:20:50 +0530

    'inital_commit'

---
+++ Customer-personality-analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project is a demo of cookiecutter usage for furthur projects as well.',
+    author='ayannu',
+    license='',
+)
